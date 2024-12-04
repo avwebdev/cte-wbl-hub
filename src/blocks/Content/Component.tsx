43766f7b95a40a89359e0308de80b5b765276bb9
Kethan Vegunta <kethan@vegunta.com>
2024-12-04 00:12:33 -0800

    fix(content-block): adjust margin on content block and condense styling in index.css into tailwind classes

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -6,8 +6,6 @@ import { cn } from "src/utilities/cn";
 
 import { CMSLink } from "../../components/Link";
 
-import "./index.css";
-
 export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
   const { columns } = props;
 
@@ -19,7 +17,7 @@ export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
   };
 
   return (
-    <div className="content-block container my-16">
+    <div className="mx-auto my-16 w-[80vw] max-w-screen-md">
       <div className="grid grid-cols-4 gap-x-16 gap-y-8 lg:grid-cols-12">
         {columns &&
           columns.length > 0 &&
@@ -28,12 +26,26 @@ export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
 
             return (
               <div
-                className={cn(`col-span-4 lg:col-span-${colsSpanClasses[size!]}`, {
-                  "md:col-span-2": size !== "full",
-                })}
+                className={cn(
+                  `col-span-4 lg:col-span-${colsSpanClasses[size!]}`,
+                  {
+                    "md:col-span-2": size !== "full",
+                  },
+                )}
                 key={index}
               >
-                {richText && <RichText content={richText} enableGutter={false} />}
+                {richText && (
+                  <RichText
+                    className={cn(
+                      "[&>h2]:text-blue-chill-700 [&>h4]:text-blaze-orange-500",
+                      "[&>h2]:mb-3 [&>h2]:mt-0 [&>h4]:mb-1",
+                      "[&>h2]:text-4xl [&>h4]:text-xl",
+                      "[&>h2]:font-medium [&>h4]:font-medium [&>h4]:capitalize",
+                    )}
+                    content={richText}
+                    enableGutter={false}
+                  />
+                )}
 
                 {enableLink && <CMSLink {...link} />}
               </div>
