f257ebdb52e0e7f746dc1a5a02974f5ba3b39ec1
Kethan Vegunta <kethan@vegunta.com>
2024-12-04 18:13:44 -0800

    redeploy

--- src/utilities/getURL.ts
+++ src/utilities/getURL.ts
@@ -7,6 +7,10 @@ export const getServerSideURL = () => {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
 
+  if (!url && process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
   if (!url) {
     url = "http://localhost:3000";
   }
