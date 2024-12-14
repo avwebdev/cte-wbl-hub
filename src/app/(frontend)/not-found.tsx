323ff14c300d2bb4625d65a8b1665a829f02a660
Kethan Vegunta <kethan@vegunta.com>
2024-12-14 12:51:11 -0800

    fix: improve styling of 404 page

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -5,10 +5,10 @@ import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <div className="container py-28">
+    <div className="flex w-full flex-col items-center gap-4 py-28 text-center">
       <div className="prose max-w-none">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
+        <h1>404</h1>
+        <p className="mt-0">This page could not be found.</p>
       </div>
       <Button asChild variant="default">
         <Link href="/">Go home</Link>
